41baaed5f3b382b204532b7645bc8298fb24893e
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-10 12:51:34 +0100

    added a mini display to show current calculation.

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const num = document.querySelectorAll(".num")
 const operation = document.querySelectorAll(".op")
-const disp = document.querySelector(".disp")
+const calcs = document.querySelector(".calc")
 const result = document.querySelector(".result")
-let calc = {num1: "0" ,num2: "", op: "", prevnum: ""} //add prevnum
+const mini = document.querySelector(".mini")
+let calc = {num1: "0" ,num2: "", op: "", prevnum: ""}
 
 result.addEventListener("click", () => showResult())
 
 num.forEach(item => {
     item.addEventListener("click", () => {
         if(calc.op == ""){
+            mini.textContent = ""
             calc.num1 = calc.num1 + item.textContent
-            disp.textContent = parseFloat(calc.num1)
+            calcs.textContent = parseFloat(calc.num1)
         }else{
             calc.num2 = calc.num2 + item.textContent
-            disp.textContent = parseFloat(calc.num2)
+            calcs.textContent = parseFloat(calc.num2)
         }
     })
 })
@@ -23,8 +25,10 @@ operation.forEach(item => {
         if(calc.num1 == "0" && calc.prevnum != ""){
             calc.num1 = calc.prevnum
             calc.op = item.textContent
+            mini.textContent = parseFloat(calc.num1) + " " + calc.op
         }else{
             calc.op = item.textContent
+            mini.textContent = parseFloat(calc.num1) + " " + calc.op
         }
     })
 })
@@ -34,28 +38,32 @@ function showResult(){
     calc.num2 = parseFloat(calc.num2)
     if(calc.op == "+"){
         res = calc.num1 + calc.num2
-        disp.textContent = res
+        calcs.textContent = res
+        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
         calc.prevnum = res
         calc.num1 = "0"
         calc.num2 = ""
         calc.op = ""
     }else if(calc.op == "-"){
         res = calc.num1 - calc.num2
-        disp.textContent = res
+        calcs.textContent = res
+        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
         calc.prevnum = res
         calc.num1 = "0"
         calc.num2 = ""
         calc.op = ""
     }else if(calc.op == "*"){
         res = calc.num1 * calc.num2
-        disp.textContent = res
+        calcs.textContent = res
+        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
         calc.prevnum = res
         calc.num1 = "0"
         calc.num2 = ""
         calc.op = ""
     }else if(calc.op == "/"){
         res = calc.num1 / calc.num2
-        disp.textContent = res
+        calcs.textContent = res
+        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
         calc.prevnum = res
         calc.num1 = "0"
         calc.num2 = ""

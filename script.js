cd297962d573a4f4c758d4d0c530bc47ef558ee8
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-10 15:26:35 +0100

    more buttons, more style.
    -added del, clear and power buttons.
    -slight style additions.

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const operation = document.querySelectorAll(".op")
 const calcs = document.querySelector(".calc")
 const result = document.querySelector(".result")
 const mini = document.querySelector(".mini")
-let calc = {num1: "0" ,num2: "", op: "", prevnum: ""}
+const delet = document.querySelector(".del")
+const ac = document.querySelector(".ac")
+const r = document.querySelector(".r")
+let calc = {num1: "0" ,num2: "0", op: "", prevnum: ""}
 
-result.addEventListener("click", () => showResult())
+result.addEventListener("click", () => operate())
+delet.addEventListener("click", () => del())
+ac.addEventListener("click", () => clearAll())
+r.addEventListener("click", () => re())
 
 num.forEach(item => {
     item.addEventListener("click", () => {
@@ -26,47 +32,108 @@ operation.forEach(item => {
             calc.num1 = calc.prevnum
             calc.op = item.textContent
             mini.textContent = parseFloat(calc.num1) + " " + calc.op
+        }else if(calc.num2 != "0"){
+            operate()
+            calc.num1 = calc.prevnum
+            calc.op = item.textContent
+            mini.textContent = calc.num1 + " " + calc.op
         }else{
             calc.op = item.textContent
             mini.textContent = parseFloat(calc.num1) + " " + calc.op
+            calcs.textContent = "0"
         }
     })
 })
 
-function showResult(){
+function add(){
+    res = calc.num1 + calc.num2
+    calcs.textContent = res
+    mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
+    calc.prevnum = res
+    calc.num1 = "0"
+    calc.num2 = "0"
+    calc.op = ""
+}
+
+function sub(){
+    res = calc.num1 - calc.num2
+    calcs.textContent = res
+    mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
+    calc.prevnum = res
+    calc.num1 = "0"
+    calc.num2 = "0"
+    calc.op = ""
+}
+
+function mult(){
+    res = calc.num1 * calc.num2
+    calcs.textContent = res
+    mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
+    calc.prevnum = res
+    calc.num1 = "0"
+    calc.num2 = "0"
+    calc.op = ""
+}
+
+function div(){
+    res = calc.num1 / calc.num2
+    calcs.textContent = res
+    mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
+    calc.prevnum = res
+    calc.num1 = "0"
+    calc.num2 = "0"
+    calc.op = ""
+}
+
+function pow(){
+    res = calc.num1 ** calc.num2
+    calcs.textContent = res
+    mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
+    calc.prevnum = res
+    calc.num1 = "0"
+    calc.num2 = "0"
+    calc.op = ""
+}
+
+function re(){
+    calcs.textContent = 1 / calcs.textContent
+    if(calc.op == ""){
+        calc.num1 = calcs.textContent
+    }else{
+        calc.num2 = calcs.textContent
+    }
+}
+
+function operate(){
     calc.num1 = parseFloat(calc.num1)
     calc.num2 = parseFloat(calc.num2)
     if(calc.op == "+"){
-        res = calc.num1 + calc.num2
-        calcs.textContent = res
-        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
-        calc.prevnum = res
-        calc.num1 = "0"
-        calc.num2 = ""
-        calc.op = ""
+        add()
     }else if(calc.op == "-"){
-        res = calc.num1 - calc.num2
-        calcs.textContent = res
-        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
-        calc.prevnum = res
-        calc.num1 = "0"
-        calc.num2 = ""
-        calc.op = ""
+        sub()
     }else if(calc.op == "*"){
-        res = calc.num1 * calc.num2
-        calcs.textContent = res
-        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
-        calc.prevnum = res
-        calc.num1 = "0"
-        calc.num2 = ""
-        calc.op = ""
+        mult()
     }else if(calc.op == "/"){
-        res = calc.num1 / calc.num2
-        calcs.textContent = res
-        mini.textContent = calc.num1 + " " + calc.op + " " + calc.num2 + " ="
-        calc.prevnum = res
-        calc.num1 = "0"
-        calc.num2 = ""
-        calc.op = ""
+        div()
+    }else if(calc.op == "**"){
+        pow()
     }
+}
+
+function del(){
+    calcs.textContent = calcs.textContent.slice(0, -1);
+    if(calc.op == ""){
+        calc.num1 = calcs.textContent
+    }else{
+        calc.num2 = calcs.textContent
+    }
+}
+
+function clearAll(){
+    calcs.textContent = "0"
+    mini.textContent = ""
+    calc.num1 = "0"
+    calc.num2 = "0"
+    calc.op = ""
+    calc.prevnum = ""
 }

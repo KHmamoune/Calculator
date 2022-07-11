cff8c0074ac19c6fa767b838b677425ccdc6c1e3
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-11 21:28:05 +0100

    more functions, bug fixes and a title.
    -added 4 new functions to the calculator.
    -fixed a bug where you could put more than 1 ".".
    -the page finally has a title.

--- script.js
+++ script.js
@@ -5,43 +5,30 @@ const result = document.querySelector(".result")
 const mini = document.querySelector(".mini")
 const delet = document.querySelector(".del")
 const ac = document.querySelector(".ac")
-const r = document.querySelector(".r")
+const sp_op = document.querySelectorAll(".sp_op")
+const dot = document.querySelector(".dot")
 let calc = {num1: "0" ,num2: "0", op: "", prevnum: ""}
 
 result.addEventListener("click", () => operate())
 delet.addEventListener("click", () => del())
 ac.addEventListener("click", () => clearAll())
-r.addEventListener("click", () => re())
+dot.addEventListener("click", () => addDot())
 
 num.forEach(item => {
     item.addEventListener("click", () => {
-        if(calc.op == ""){
-            mini.textContent = ""
-            calc.num1 = calc.num1 + item.textContent
-            calcs.textContent = parseFloat(calc.num1)
-        }else{
-            calc.num2 = calc.num2 + item.textContent
-            calcs.textContent = parseFloat(calc.num2)
-        }
+        addNum(item.textContent)
     })
 })
 
 operation.forEach(item => {
     item.addEventListener("click", () => {
-        if(calc.num1 == "0" && calc.prevnum != ""){
-            calc.num1 = calc.prevnum
-            calc.op = item.textContent
-            mini.textContent = parseFloat(calc.num1) + " " + calc.op
-        }else if(calc.num2 != "0"){
-            operate()
-            calc.num1 = calc.prevnum
-            calc.op = item.textContent
-            mini.textContent = calc.num1 + " " + calc.op
-        }else{
-            calc.op = item.textContent
-            mini.textContent = parseFloat(calc.num1) + " " + calc.op
-            calcs.textContent = "0"
-        }
+        addOp(item.textContent)
+    })
+})
+
+sp_op.forEach(item => {
+    item.addEventListener("click", () => {
+        addSpOp(item.textContent)
     })
 })
 
@@ -95,7 +82,7 @@ function pow(){
     calc.op = ""
 }
 
-function re(){
+function inverse(){
     calcs.textContent = 1 / calcs.textContent
     if(calc.op == ""){
         calc.num1 = calcs.textContent
@@ -104,19 +91,58 @@ function re(){
     }
 }
 
-function operate(){
-    calc.num1 = parseFloat(calc.num1)
-    calc.num2 = parseFloat(calc.num2)
-    if(calc.op == "+"){
-        add()
-    }else if(calc.op == "-"){
-        sub()
-    }else if(calc.op == "*"){
-        mult()
-    }else if(calc.op == "/"){
-        div()
-    }else if(calc.op == "**"){
-        pow()
+function factor(){
+    let fact = 1
+    if(calc.op == ""){
+        for(i=1; i<=calc.num1; i++){
+            fact = fact * i
+        }
+        calc.num1 = fact
+    }else{
+        for(i=1; i<=calc.num1; i++){
+            fact = fact * i
+        }
+        calc.num2 = fact
+    }
+    calcs.textContent = fact
+}
+
+function root(){
+    calcs.textContent = Math.sqrt(calcs.textContent)
+    if(calc.op == ""){
+        calc.num1 = calcs.textContent
+    }else{
+        calc.num2 = calcs.textContent
+    }
+}
+
+function negative(){
+    calcs.textContent = -1 * calcs.textContent
+    if(calc.op == ""){
+        calc.num1 = calcs.textContent
+    }else{
+        calc.num2 = calcs.textContent
+    }
+}
+
+function square(){
+    calcs.textContent = calcs.textContent ** 2
+    if(calc.op == ""){
+        calc.num1 = calcs.textContent
+    }else{
+        calc.num2 = calcs.textContent
+    }
+}
+
+function addDot(){
+    if(calc.num1.includes(".") == false){
+        if(calc.op == ""){
+            calc.num1 = calc.num1 + dot.textContent
+            calcs.textContent = parseFloat(calc.num1)
+        }else{
+            calc.num2 = calc.num2 + dot.textContent
+            calcs.textContent = parseFloat(calc.num2)
+        }
     }
 }
 
@@ -136,4 +162,64 @@ function clearAll(){
     calc.num2 = "0"
     calc.op = ""
     calc.prevnum = ""
+}
+
+function addNum(number){
+    if(calc.op == ""){
+        mini.textContent = ""
+        calc.num1 = calc.num1 + number
+        calcs.textContent = parseFloat(calc.num1)
+    }else{
+        calc.num2 = calc.num2 + number
+        calcs.textContent = parseFloat(calc.num2)
+    }
+}
+
+function addOp(operator){
+    if(calc.num1 == "0" && calc.prevnum != ""){
+        calc.num1 = calc.prevnum
+        calc.op = operator
+        mini.textContent = parseFloat(calc.num1) + " " + calc.op
+    }else if(calc.num2 != "0"){
+        operate()
+        calc.num1 = calc.prevnum
+        calc.op = operator
+        mini.textContent = calc.num1 + " " + calc.op
+    }else{
+        calc.op = operator
+        mini.textContent = parseFloat(calc.num1) + " " + calc.op
+        calcs.textContent = "0"
+    }
+}
+
+function addSpOp(spOp){
+    if(spOp == "1/x"){
+        inverse()
+    }else if(spOp == "x!"){
+        factor()
+    }else if(spOp == "√"){
+        root()
+    }else if(spOp == "+/-"){
+        negative()
+    }else if(spOp == "x²"){
+        square()
+    }
+}
+
+function operate(){
+    calc.num1 = parseFloat(calc.num1)
+    calc.num2 = parseFloat(calc.num2)
+    if(calc.op == "/" && calc.num2 == 0){
+        alert("not exactly sure what are you trying to prove here but it isn't saying much about you math abilities")
+    }else if(calc.op == "+"){
+        add()
+    }else if(calc.op == "-"){
+        sub()
+    }else if(calc.op == "x"){
+        mult()
+    }else if(calc.op == "/"){
+        div()
+    }else if(calc.op == "^"){
+        pow()
+    }
 }
